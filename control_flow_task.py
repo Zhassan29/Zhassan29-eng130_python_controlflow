5b974aea829a4b63aa86a9a797c56452b5b252d4
maiza <maiza@maizas-MacBook-Air.local>
2022-10-12 15:48:15 +0100

    updated

--- control_flow_task.py
+++ control_flow_task.py
@@ -7,7 +7,14 @@
 # user must not be allowed to enter age over 117 years
 
 print("hi, please input age")
-age = input()
-if age == ">= 18":
+age = int(input(""))
+if age >= 18:
     print("allowed to watch all movies")
-elif age == ">= 16"
+elif 16 <= age:
+    print("allowed movies starting from 16")
+elif 12 <= age:
+    print("parental guidance is required")
+elif age >= 117:
+    print("age must be lower than 117")
+else:
+    print("please enter your age again")
